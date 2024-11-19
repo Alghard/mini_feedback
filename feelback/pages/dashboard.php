1927789ac7126285da2a6bf9b8997ab3c38d740d
Victor <62471302+DiscoMajor@users.noreply.github.com>
2024-11-19 16:11:27 +0100

    Changed all the html pages into php pages. Dashboard page WIP

---
+++ feelback/pages/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FeelBack - Questionnaire de Satisfaction</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+<body>
+        <!-- Header -->
+        <header>
+            <img src="../assets/logo.png" alt="Logo FeelBack" class="logo">
+            <h1>FeelBack</h1>
+            <p class="subtitle">Le questionnaire de satisfaction</p>
+        </header>
+</body>
+</html>
